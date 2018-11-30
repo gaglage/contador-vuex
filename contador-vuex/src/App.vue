dffdddf65129388c19feb7a1022759901c69acee
gabrielalvarez <gabrielalvarez@dorna.com>
2018-11-30 13:13:33 +0100

    hello

--- contador-vuex/src/App.vue
+++ contador-vuex/src/App.vue
@@ -1,18 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>{{ message }}</p>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      message:'Hola!'
+    }
   }
+
 }
 </script>
 <style lang="stylus">
